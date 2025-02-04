b65df0ff216ff3b964b73b88bc8157dfca9fffbe
Ian Dela Cruz <ian.delacruz@edgeworks.com.sg>
2025-02-04 15:07:57 +0800

    Auto update app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
