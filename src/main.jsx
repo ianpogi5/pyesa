5e66c668a7f66e9bfeed540824b357bdca11d9ff
Ian Dela Cruz <ian.delacruz@edgeworks.com.sg>
2025-02-07 08:59:28 +0800

    Get Periodic updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,29 @@ import { BrowserRouter } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
 
-registerSW({ immediate: true });
+const intervalMS = 60 * 60 * 1000;
+
+registerSW({
+  onRegisteredSW(swUrl, r) {
+    const update = async () => {
+      if (r.installing || !navigator) return;
+
+      if ("connection" in navigator && !navigator.onLine) return;
+
+      const resp = await fetch(swUrl, {
+        cache: "no-store",
+        headers: {
+          cache: "no-store",
+          "cache-control": "no-cache",
+        },
+      });
+
+      if (resp?.status === 200) await r.update();
+    };
+    r && update();
+    r && setInterval(update, intervalMS);
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
